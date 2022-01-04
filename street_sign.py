9e3c01c602f501c5573fadef32471d3106e8f5de
ariffrahimin <57780003+ariffrahimin@users.noreply.github.com>
2022-01-04 13:12:29 +0800

    modify test data

--- street_sign.py
+++ street_sign.py
@@ -1,8 +1,13 @@
 from sklearn.model_selection import train_test_split
-from my_utils import split_data
+from my_utils import split_data, order_test_set
 
 if __name__=="__main__":
-    path_to_data = "E:\\Projects\\Python\\trafficsigndetection\\traffic_sign_data\\Train"
-    path_to_save_train = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\train"
-    path_to_save_val = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\val"
-    split_data(path_to_data, path_to_save_train=path_to_save_train, path_to_save_val=path_to_save_val)
+    if False:
+        path_to_data = "E:\\Projects\\Python\\trafficsigndetection\\traffic_sign_data\\Train"
+        path_to_save_train = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\train"
+        path_to_save_val = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\val"
+        split_data(path_to_data, path_to_save_train=path_to_save_train, path_to_save_val=path_to_save_val)
+
+    path_to_images = "E:\\Projects\\Python\\trafficsigndetection\\traffic_sign_data\\Test"
+    path_to_csv = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\Test.csv"
+    order_test_set(path_to_images, path_to_csv)
