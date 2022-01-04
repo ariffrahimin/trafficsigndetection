9df6594e51f3ab2f811d0980cf08d01420af2f8c
ariffrahimin <57780003+ariffrahimin@users.noreply.github.com>
2022-01-04 12:38:06 +0800

    create training and val folder

--- street_sign.py
+++ street_sign.py
@@ -1,6 +1,8 @@
 import os
 import glob
 from sklearn.model_selection import train_test_split
+import shutil
+
 def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.1):
     folders = os.listdir(path_to_data)
 
@@ -12,5 +14,24 @@ def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.
 
         for x in x_train:
 
-            basename = os.path.basename(x)
             path_to_folder = os.path.join(path_to_save_train, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+        for x in x_val:
+
+            path_to_folder = os.path.join(path_to_save_val, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+if __name__=="__main__":
+    path_to_data = "E:\\Projects\\Python\\trafficsigndetection\\traffic_sign_data\\Train"
+    path_to_save_train = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\train"
+    path_to_save_val = "E:\\Projects\Python\\trafficsigndetection\\traffic_sign_data\\training_data\\val"
+    split_data(path_to_data, path_to_save_train=path_to_save_train, path_to_save_val=path_to_save_val)
