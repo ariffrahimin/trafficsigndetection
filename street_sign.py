61d28f9da1a40a0aed5e4ebb694fcef476095b7e
ariffrahimin <57780003+ariffrahimin@users.noreply.github.com>
2022-01-04 12:19:21 +0800

    new folder creation

--- street_sign.py
+++ street_sign.py
@@ -1,6 +1,16 @@
 import os
+import glob
+from sklearn.model_selection import train_test_split
 def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.1):
     folders = os.listdir(path_to_data)
 
     for folder in folders:
-        full_path = 
+        full_path = os.path.join(path_to_data, folder)
+        images_paths = glob.glob(os.path.join(full_path, '*.png'))
+
+        x_train, x_val = train_test_split(images_paths,test_size=split_size)
+
+        for x in x_train:
+
+            basename = os.path.basename(x)
+            path_to_folder = os.path.join(path_to_save_train, folder)
